4a36f22336a445a0e072d3a441252d85b9858656
GAOTANG SHEN <gaotangshen@gmail.com>
2018-01-02 14:19:46 -0500

    Fixed small bug with no exist city

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,9 @@
 import { FEACH_WEATHER } from "../actions/index";
 
 export default function (state = [], action) {
+  if (action.error) {
+    return state;
+  }
   switch(action.type) {
     case FEACH_WEATHER:
       return [action.payload.data, ...state];
