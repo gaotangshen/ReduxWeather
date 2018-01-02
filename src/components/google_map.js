a11b8d58eaf8f4f4150797cd082dde0e637a06ff
GAOTANG SHEN <gaotangshen@gmail.com>
2018-01-01 19:23:27 -0500

    use react google map package

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
+import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
 
-class GoogleMap extends Component {
-  // A lifecycle method thats gets called automaticlly 
-  // after this component has been rendered on the screen.
-  componentDidMount() {
-    new google.maps.Map(this.refs.map, {
-      zoom: 12,
-      center: {
-        lat: this.props.lat,
-        lng: this.props.lon
+export default (props) => {
+  return (
+    <GoogleMapLoader
+      containerElement={ <div style={{height: '100%'}}/>}
+      googleMapElement={
+        <GoogleMap defaultZoom={12} defaultCenter={{lat:props.lat, lng: props.lon}}/>
       }
-    });
-  }
-
-  render() {
-    // this.refs.map
-    return <div ref="map" />;
-  }
+    />
+  );
 }
 
-export default GoogleMap;
+
+// class GoogleMap extends Component {
+//   // A lifecycle method thats gets called automaticlly 
+//   // after this component has been rendered on the screen.
+//   componentDidMount() {
+//     new google.maps.Map(this.refs.map, {
+//       zoom: 12,
+//       center: {
+//         lat: this.props.lat,
+//         lng: this.props.lon
+//       }
+//     });
+//   }
+
+//   render() {
+//     // this.refs.map
+//     return <div ref="map" />;
+//   }
+// }
+
+// export default GoogleMap;
